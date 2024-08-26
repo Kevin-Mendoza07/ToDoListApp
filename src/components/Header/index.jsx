23c492d8462eb48d4be1e70ef5d150e93c1b5ef9
Kevin-Mendoza07 <kevingfx71@gmail.com>
2024-08-26 00:08:25 -0600

    feat: Uploading todolistapp with react + vite

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,29 @@
+import todoLogo from '../../assets/todoLogo.svg';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+import styles from './header.module.css';
+import { useState } from 'react';
+
+export function Header({ onAddTask }) {
+    const [title, setTitle] = useState('');
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        onAddTask(title);
+        setTitle('');
+    }
+
+    function onChangeTitle(event) {
+        setTitle(event.target.value);
+    }
+
+    return (
+        <header className={styles.header}>
+            <img src={todoLogo} />
+
+            <form onSubmit={handleSubmit} className={styles.newTaskForm}>
+                <input placeholder="add a new task" type="text" value={title} onChange={onChangeTitle}></input>
+                <button>Create <AiOutlinePlusCircle size={20} /></button>
+            </form>
+        </header>
+    )
+}
